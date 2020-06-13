82bf9d99d143ca2cfe7c8bff50a3ef2756cb51ee
alendass <alendass@central.uh.edu>
2020-06-13 15:22:03 -0500

    Seventh commit

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
-s1 = {1, 2}
-print(s1)
-s2 = {3, 2, 6, 7}
-print(s2)
+# Amaury Lendasse
+import numpy as np
+import matplotlib.pyplot as plt
 
-s3 = s1 | s2
-print(s3)
+
+# generating x and y
+sizex = [int(1e3), 1]
+x = np.random.uniform(-5, 5, sizex)
+y = np.sin(np.random.uniform(0, 2, 1) *x) + np.sin(np.random.uniform(1, 3, 1) * x) + np.sin(np.random.uniform(1, 5, 1) * x)
+noise = np.random.normal(0, 0.005, sizex)
+y = y + noise
+
+# normalization
+m = np.mean(x)
+s = np.std(x)
+x = (x - m) / s
+
+unitvector = np.ones((sizex[0], 1))
+x = np.concatenate((x, unitvector), axis=1)
+sizex[1] += 1
+
+
+
+# generating w
+sizew = 100
+w = np.random.normal(0, 2, [sizex[1], sizew])
+
+for i in range(0, sizew + 1, 5):
+    # calculating H
+    H = np.matmul(x, w[:, :i])
+    H = np.tanh(H)
+    H = np.concatenate((H, unitvector), axis=1)
+
+    # solving system
+    HH = np.matmul(np.transpose(H), H)
+    HH = HH + np.identity(np.size(HH, 1)) * 0.0000001
+    HY = np.matmul(np.transpose(H), y)
+    A = np.linalg.solve(HH, HY)
+    yh = np.matmul(H, A)
+
+    # calculating MSE
+    mse = ((y - yh) ** 2).mean(axis=0)
+    print('MSE is', mse)
+
+    # plotting
+    # plt.plot(x[:,0], y, 'b.', x[:,0], yh, 'r.')
+    plt.plot(x[:,0], y, 'b.')
+    plt.plot(x[:,0], yh, 'r.')
+    plt.ion()
+    plt.show()
+    plt.pause(.1)
+
+
---
+++ main2.py
@@ -0,0 +1,19 @@
+# Amaury Lendasse
+import time
+
+myrange = list(range(3, int(1e5), 2))
+
+start_time = time.time()
+for i in myrange:
+    if i>(myrange[-1]/i):
+        break
+    for j in range(i,(myrange[-1]+1)//i+1,2):
+        m=j*i
+        if (m in myrange):
+            myrange.remove(m)
+
+elapsed_time = time.time() - start_time
+print(elapsed_time)
+
+print(len(myrange))
+print(myrange)
